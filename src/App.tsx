c840cc6438af202ebbf809e93b87a1466ec717b2
Florian Rädiker <github@florian-raediker.de>
2022-09-07 15:16:15 +0200

    Add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,13 @@ import './App.css';
 export function App() {
   return <>
     <header>
-      <NavLink to="/">
+      <Link to="/">
         <h1 className="header">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-door-fill" viewBox="0 0 16 16">
-            <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z" />
-          </svg>{" "}
+          <svg version="1.1" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg"><rect width="64" height="64" ry="12" /><rect x="8" y="8" width="12" height="12" fill="#b59f3b" /><g fill="#fff"><rect x="25.981" y="8" width="12" height="12" /><rect x="43.963" y="8" width="12" height="12" /><rect x="8" y="25.981" width="12" height="12" /><rect x="25.981" y="25.981" width="12" height="12" /></g><g fill="#538d4e"><rect x="43.963" y="25.981" width="12" height="12" /><rect x="43.963" y="43.963" width="12" height="12" /><rect x="25.981" y="43.963" width="12" height="12" /><rect x="8" y="43.963" width="12" height="12" /></g></svg>
+          {" "}
           World of Wordle
         </h1>
-      </NavLink>
+      </Link>
     </header>
     <main className="main">
       <Outlet />
