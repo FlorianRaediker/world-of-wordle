9614893daa395137377fbcebd252c9544bc536d4
Florian Rädiker <github@florian-raediker.de>
2022-09-19 15:28:19 +0200

    Add title

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ComponentType, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App, AppNavigation } from "./App";
@@ -9,16 +9,32 @@ import WordSpy from "./word-spy/WordSpy";
 import GamesOverview from "./games-overview/games-overview";
 
 
+function withTitle<T>(Component: ComponentType<T>, title: string) {
+  return function (props: T) {
+    useEffect(() => {
+      document.title = title;
+    });
+    return <Component {...props} />;
+  }
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+const AppNavigationWithTitle = withTitle(AppNavigation, "World of Wordle");
+const GamesOverviewWithTitle = withTitle(GamesOverview, "Games | World of Wordle");
+const WordSpyWithTitle = withTitle(WordSpy, "Word Spy | World of Wordle");
+const ReconstructorWithTitle = withTitle(Reconstructor, "Wordle Reconstructor | World of Wordle");
+
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route index element={<AppNavigation />} />
-          <Route path="games" element={<GamesOverview />} />
-          <Route path="word-spy" element={<WordSpy />} />
-          <Route path="reconstructor" element={<Reconstructor />} />
+          <Route index element={<AppNavigationWithTitle />} />
+          <Route path="games" element={<GamesOverviewWithTitle />} />
+          <Route path="word-spy" element={<WordSpyWithTitle />} />
+          <Route path="reconstructor" element={<ReconstructorWithTitle />} />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
